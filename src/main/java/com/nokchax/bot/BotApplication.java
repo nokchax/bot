97149d1a7aad13c95987f0b678a3228945740884
nokchax <nokchax@gmail.com>
2020-07-12 22:02:40 +0900

    feat: add telegram init code

--- src/main/java/com/nokchax/bot/BotApplication.java
+++ src/main/java/com/nokchax/bot/BotApplication.java
@@ -2,11 +2,14 @@ package com.nokchax.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class BotApplication {
 
     public static void main(String[] args) {
+        ApiContextInitializer.init(); // init telegram bot
+
         SpringApplication.run(BotApplication.class, args);
     }
 
