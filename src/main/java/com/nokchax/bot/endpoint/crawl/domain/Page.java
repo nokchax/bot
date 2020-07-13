c561fcae349205bd13b7dbe0ecb5229cd2b29e4a
nokchax <nokchax@gmail.com>
2020-07-13 21:57:11 +0900

    feat: add last page info and last crawl time and crawl term

--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/Page.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/Page.java
@@ -1,5 +1,7 @@
 package com.nokchax.bot.endpoint.crawl.domain;
 
+import java.time.LocalDateTime;
+
 public class Page {
     private int id;
     private String name; // title?
@@ -7,4 +9,7 @@ public class Page {
     private String pathType; // xpath인지 아니면 <body> <table> <td>처럼 tag를 타고 들어가는 path인지.. id인지 class인지..
     private String path; // 실 path를 담는다
     private String alramType; // 어느 경우에 알람을 할 것인지
+    private String lastPage; // 마지막 페이지 크롤 정보
+    private String crawlTerm; // 크롤하는 텀
+    private LocalDateTime lastCrawlTime; // 마지막 크롤 시점 -> history table도 만드는게 좋을듯
 }
