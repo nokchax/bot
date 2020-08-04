2515b091a059be49f620ae5f252272436f8985ec
nokchax <nokchax@gmail.com>
2020-08-04 22:00:28 +0900

    feat: change crawl term to cron expression

--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/Crawl.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/Crawl.java
@@ -16,11 +16,20 @@ public class Crawl {
     private String name;
     private boolean isActive;
     private String lastCrawlInfo; // 여기 아니면 history?
-    private LocalDateTime lastCrawlTime;
     private long crawlTerm;
-    private TermType termType;
+    private String cronExp;
+    private LocalDateTime nextCrawlTime;
+    private LocalDateTime lastCrawlTime;
 
     @OrderBy("id desc")
     @OneToMany
     private List<CrawlHistory> histories = new ArrayList<>();
+
+    /* example
+        private void test() {
+            CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator("");
+            Date next = cronSequenceGenerator.next(new Date());
+            LocalDateTime localDateTime = next.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        }
+     */
 }
