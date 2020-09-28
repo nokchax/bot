d67ebbf26287c3fbe27363417e855ac1d5a851e3
nokchax <nokchax@gmail.com>
2020-09-28 23:12:02 +0900

    comment: explain filter type enum

--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/FilterType.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/FilterType.java
@@ -1,10 +1,10 @@
 package com.nokchax.bot.endpoint.crawl.domain;
 
 public enum FilterType {
-    MATCH_EXACT,
-    EXCLUDE_EXACT,
-    CONTAIN,
-    START_WITH,
-    END_WITH,
-    EXCLUDE;
+    MATCH_EXACT, // 정확히 일치, equals
+    EXCLUDE_EXACT, // 포함하면 안됨, not equals
+    CONTAIN, // contains
+    START_WITH, // startWith
+    END_WITH, // endWith
+    EXCLUDE // not contains
 }
