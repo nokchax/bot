7d6f6921f5f163a6da5531826a4f4c14daf24d6d
nokchax <nokchax@gmail.com>
2020-10-03 23:37:34 +0900

    design: alarm media and extension

--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/NotificationTarget.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/NotificationTarget.java
@@ -3,10 +3,14 @@ package com.nokchax.bot.endpoint.crawl.domain;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class NotificationTarget {
     // 알람을 받을 대상에 대한 정보를 담고 있음
-    // alaram type (mail, phone, messenger)
+    // alarm media 에 따라 필요한 정보가 달라지므로 NotificationTarget 테이블을 상속하여 매체에 따라 다른 테이블을 사용하는 방식을 사용할 것
+    @Id
+    private int id;
+    private AlarmMedia alarmMedia;
 }
