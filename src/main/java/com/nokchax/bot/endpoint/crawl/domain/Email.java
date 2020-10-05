b86f1040233e8f1aef981bc4c1f31bd318eb5e17
nokchax <nokchax@gmail.com>
2020-10-05 23:13:57 +0900

    feat: add jpa annotation

--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/Email.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/Email.java
@@ -1,4 +1,12 @@
 package com.nokchax.bot.endpoint.crawl.domain;
 
+import lombok.Data;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@DiscriminatorValue("email")
 public class Email extends NotificationTarget {
 }
--- src/main/java/com/nokchax/bot/endpoint/crawl/domain/NotificationTarget.java
+++ src/main/java/com/nokchax/bot/endpoint/crawl/domain/NotificationTarget.java
@@ -2,11 +2,12 @@ package com.nokchax.bot.endpoint.crawl.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@DiscriminatorColumn(columnDefinition = "type")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class NotificationTarget {
     // 알람을 받을 대상에 대한 정보를 담고 있음
     // alarm media 에 따라 필요한 정보가 달라지므로 NotificationTarget 테이블을 상속하여 매체에 따라 다른 테이블을 사용하는 방식을 사용할 것
